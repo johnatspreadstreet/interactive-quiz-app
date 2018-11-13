4949f1576c243d0cd26a31409cf427816680a946
John Young <john.young@spreadstreet.io>
2018-11-13 18:52:05 -0500

    first commit

---
+++ js/thor.js
@@ -0,0 +1,39 @@
+// 1) What is the name of Thor's enchanted hammer? Link	URL
+// ASGARD	
+// FENRIS	
+// GUNGNIR	
+// MJÖLNIR	
+
+// 2) Who is Thor's father?
+// 3) In Norse mythology, what event was foretold to result in the death of Thor?
+// 4) Which of Thor's comrades is the son of nine mothers?
+// 5) Who was the first Marvel character outside of the Norse pantheon to be deemed worthy to wield Thor's hammer?
+// 6) What is responsible for Thor's immortality?
+// 7) What animals pull Thor's chariot?
+// 8) What part of Thor's costume is enchanted?
+// 9) What is the name of Thor's mortal love interest?
+// 10) Thor was a founding member of what superhero team?
+
+'use strict';
+
+const THORSTORE = [
+  {
+    number: 1,
+    text: '1) What is the name of Thor\'s enchanted hammer?',
+    sel1: 'ASGARD',
+    sel2: 'FENRIS',
+    sel3: 'GUNGNIR',
+    sel4: 'MJÖLNIR',
+    ans: sel4
+  },
+
+  {
+    number: 2,
+    text: '2) Who is Thor\'s father?',
+    sel1: 'BALDER',
+    sel2: 'ODIN',
+    sel3: 'FENRIS',
+    sel4: 'LOKI',
+    ans: sel2
+  }
+];
